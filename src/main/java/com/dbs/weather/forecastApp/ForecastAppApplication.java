65724ea6a3ea64c5b6bcfce1c88bfee526c55534
priyanga220 <priyanga220@gmail.com>
2020-07-04 11:20:10 +0800

    InitialCOmmit_SptingInitGenerated

---
+++ src/main/java/com/dbs/weather/forecastApp/ForecastAppApplication.java
@@ -0,0 +1,13 @@
+package com.dbs.weather.forecastApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForecastAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForecastAppApplication.class, args);
+	}
+
+}
